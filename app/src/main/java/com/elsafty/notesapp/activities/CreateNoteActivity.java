272e79ff1ad4e26733501cf069a9edb3307a8d63
Ahmed Elsafty <ahmed0elsafty@users.noreply.github.com>
2020-10-27 03:12:52 +0200

    Create & Save Note

--- app/src/main/java/com/elsafty/notesapp/activities/CreateNoteActivity.java
+++ app/src/main/java/com/elsafty/notesapp/activities/CreateNoteActivity.java
@@ -1,25 +1,95 @@
 package com.elsafty.notesapp.activities;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.elsafty.notesapp.R;
+import com.elsafty.notesapp.database.NoteDatabase;
+import com.elsafty.notesapp.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    @BindView(R.id.inputNoteTitle)
+    EditText inputNoteTitle;
+    @BindView(R.id.textDateTime)
+    TextView textDateTime;
+    @BindView(R.id.inputNoteSubtitle)
+    EditText inputNoteSubtitle;
+    @BindView(R.id.imageSave)
+    ImageView imageSave;
+    @BindView(R.id.imageBack)
+    ImageView imageBack;
+    @BindView(R.id.subtitle_indecator)
+    View subtitleIndecator;
+    @BindView(R.id.inputNote)
+    EditText inputNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
-        ImageView imageView = findViewById(R.id.imageBack);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
+        textDateTime.setText(new SimpleDateFormat("EEEE,dd MMMM yyyy HH:mm a",
+                Locale.getDefault()).format(new Date()));
+    }
+
+    private void saveNote() {
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty() &&
+                inputNote.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString().trim());
+        note.setSubtitle(inputNoteSubtitle.getText().toString().trim());
+        note.setDateTime(textDateTime.getText().toString());
+        note.setNoteText(inputNote.getText().toString().trim());
+        class InsertNoteasyncTask extends AsyncTask<Void, Void, Void> {
+
             @Override
-            public void onClick(View view) {
-                onBackPressed();
+            protected Void doInBackground(Void... voids) {
+                NoteDatabase.getNoteDatabase(getApplication()).noteDao().insertNote(note);
+                return null;
             }
-        });
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+        new InsertNoteasyncTask().execute();
+    }
+
+    @OnClick({R.id.imageSave, R.id.imageBack})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.imageSave:
+                saveNote();
+                break;
+            case R.id.imageBack:
+                onBackPressed();
+                break;
+        }
     }
 }
